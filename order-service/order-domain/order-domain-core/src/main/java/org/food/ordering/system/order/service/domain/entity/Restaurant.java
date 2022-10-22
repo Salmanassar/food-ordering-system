ea2c76cf270ff9bbc93c8c8677ce4e38af3a1c90
salmanassar <folknerolek@gmail.com>
2022-10-22 19:06:10 +0200

    writing tests

--- order-service/order-domain/order-domain-core/src/main/java/org/food/ordering/system/order/service/domain/entity/Restaurant.java
+++ order-service/order-domain/order-domain-core/src/main/java/org/food/ordering/system/order/service/domain/entity/Restaurant.java
@@ -1,9 +1,6 @@
 package org.food.ordering.system.order.service.domain.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.food.ordering.system.order.system.domain.entity.AggregateRoot;
 import org.food.ordering.system.order.system.domain.valueobject.RestaurantId;
 
@@ -11,17 +8,50 @@ import java.util.List;
 
 @Getter
 @Setter
-@Builder
-@AllArgsConstructor
 public class Restaurant extends AggregateRoot<RestaurantId> {
     private final List<Product> products;
-
-    private RestaurantId restaurantId;
     private boolean active;
 
+    private Restaurant(Builder builder) {
+        super.setId(builder.restaurantId);
+        products = builder.products;
+        active = builder.active;
+    }
 
     public boolean isActive() {
         return active;
     }
 
+    public static Builder builder() {
+        return new Builder();
+    }
+
+
+    public static final class Builder {
+        private RestaurantId restaurantId;
+        private List<Product> products;
+        private boolean active;
+
+        private Builder() {
+        }
+
+        public Builder restaurantId(RestaurantId val) {
+            restaurantId = val;
+            return this;
+        }
+
+        public Builder products(List<Product> val) {
+            products = val;
+            return this;
+        }
+
+        public Builder active(boolean val) {
+            active = val;
+            return this;
+        }
+
+        public Restaurant build() {
+            return new Restaurant(this);
+        }
+    }
 }
